857e9be71184772fb88fad84d27355f14d547766
Rasmus Hammar <Rasmus.Hammar@gmx.com>
2026-01-21 12:00:31 +0100

    Knowledge based systems Lab 1 submitted.

--- 2026 VT/Knowledge_based_systems/Lab_1/clustering.R
+++ 2026 VT/Knowledge_based_systems/Lab_1/clustering.R
@@ -4,7 +4,7 @@
 
 # in this assignment use a 'cluster' package
 # install and load library
-install.packages("cluster") # if successful, do it only once
+# install.packages("cluster") # if successful, do it only once
 library(cluster)
 
 ### TASK 1 - check and prepare the data ###
@@ -30,16 +30,23 @@ str(iris$Species)
 
 # choose two features for the analysis e.g.
 f1 <- "Sepal.Length"
-f2 <- "Petal.Length"
+f2 <- "Sepal.Width"
 features <- c(f1, f2)
-selectedIris <- iris[,features] # or just iris[features]
+selectedIris <- iris[, features] # or just iris[features]
 
 ### TASK 2 - perform k-means clustering ###
 fitKmeans <- kmeans(selectedIris, specsNum, iter.max = 100)
-# plot the result of k-means clustering 
+# plot the result of k-means clustering
 # Fig.1
-clusplot(selectedIris, fitKmeans$cluster, color=TRUE, shade=TRUE,
-         labels=0, lines=0, main = "k-means clustering")
+clusplot(
+  selectedIris,
+  fitKmeans$cluster,
+  color = TRUE,
+  shade = TRUE,
+  labels = 0,
+  lines = 0,
+  main = "k-means clustering"
+)
 
 ### TASK 3 - perform hierarchical clustering ###
 d <- dist(selectedIris) # estimate distance
@@ -47,29 +54,54 @@ fitHier <- hclust(d, method = "ward.D")
 clusterCut <- cutree(fitHier, specsNum)
 
 # Fig.2a
-clusplot(selectedIris, clusterCut, color=TRUE, shade=TRUE,
-         labels=0, lines=0, main = "hierarchical clustering")
+clusplot(
+  selectedIris,
+  clusterCut,
+  color = TRUE,
+  shade = TRUE,
+  labels = 0,
+  lines = 0,
+  main = "hierarchical clustering"
+)
 # Fig.2b
 dend <- as.dendrogram(fitHier)
 nodeParams <- list(pch = c(NA, 18), cex = 0.5, col = "darkgray")
-plot(dend, type = "rectangle", nodePar = nodeParams, leaflab = "none", main = "hierarchical clustering")
+plot(
+  dend,
+  type = "rectangle",
+  nodePar = nodeParams,
+  leaflab = "none",
+  main = "hierarchical clustering"
+)
 rect.hclust(fitHier, k = 3, border = "darkorange")
 
 ### TASK 3 - compare k-means and hierarchical clustering
 
 # calculate centroids for the hierarchical clustering
 # here, we are using aggregate function, read more about how it works using ?aggregate
-hierCentr <- aggregate(data.frame(clusterCut, selectedIris), by = list(clusterCut), FUN = mean)
+hierCentr <- aggregate(
+  data.frame(clusterCut, selectedIris),
+  by = list(clusterCut),
+  FUN = mean
+)
 
 # display both clustering methods on single plot
-opar2 <- par(mfrow = c(1,2)) # (Figure.3)
+opar2 <- par(mfrow = c(1, 2)) # (Figure.3)
 # k-means
 plot(selectedIris, type = "n", main = "k-means clustering")
-text(selectedIris, labels = iris$Species, col = c("orangered", "limegreen", "dodgerblue")[fitKmeans$cluster])
+text(
+  selectedIris,
+  labels = iris$Species,
+  col = c("orangered", "limegreen", "dodgerblue")[fitKmeans$cluster]
+)
 points(fitKmeans$centers[, features], col = "yellow", pch = 15, cex = 1.5)
 # hierarchical
 plot(selectedIris, type = "n", main = "hierarchical clustering")
-text(selectedIris, labels = iris$Species, col = c("darkviolet", "darkorange", "seagreen4")[clusterCut])
+text(
+  selectedIris,
+  labels = iris$Species,
+  col = c("darkviolet", "darkorange", "seagreen4")[clusterCut]
+)
 points(hierCentr[, features], col = "yellow", pch = 15, cex = 1.5)
 par(opar2)
 
